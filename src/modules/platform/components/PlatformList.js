1cf0f354e0fdc314710fb8afd124af692c2c1649
Gabe Hargrave <rinkrinkerfink@gmail.com>
2016-07-19 09:28:22 -0700

    Display "no platforms" message for empty list

--- src/modules/platform/components/PlatformList.js
+++ src/modules/platform/components/PlatformList.js
@@ -4,9 +4,16 @@ import PlatformDetail from './PlatformDetail';
 
 
 const PlatformList = ({platforms}) => {
+  let count = platforms.length;
   return (
     <div>
-      {platforms.map(platform =>
+      {/* message shown when user has no platforms */}
+      {!count &&
+        <h4>You have not added any platforms yet.</h4>
+      }
+
+      {/* list shown when user has 1 or more platforms */}
+      {!!count && platforms.map(platform =>
         <PlatformDetail key={platform.id} platform={platform}/>
       )}
     </div>
