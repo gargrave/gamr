6f423e3eddffcf93d90513e52aae7e28adba6332
Gabe Hargrave <rinkrinkerfink@gmail.com>
2016-08-11 10:02:36 -0700

    Update 'About' and 'Home' pages

--- src/modules/common/AboutPage.js
+++ src/modules/common/AboutPage.js
@@ -6,7 +6,33 @@ import {APP_TITLE} from '../../constants/appConfig';
 const AboutPage = () => {
   return (
     <div>
-      <h2>About {APP_TITLE}</h2>
+      <h3>About {APP_TITLE}</h3>
+      <hr/>
+
+      <p>
+        <strong>Gamr</strong> is a simple app to keep track of what games I am playing, and when.
+      </p>
+
+      <p>
+        It uses React with Redux on the front-end, and Firebase on the back-end for authentication and DB (along with a handful of other smaller JS and CSS libraries).
+      </p>
+
+      <p>
+        For more info on myself or any of the technologies, check the links below:
+      </p>
+      <br/>
+
+      <div className="list-group">
+        <a className="list-group-item" href="https://github.com/gargrave" target="_blank">
+          My GitHub Profile
+        </a>
+        <a className="list-group-item" href="https://github.com/coryhouse/react-slingshot" target="_blank">
+          React Slingshot (React Project Boilerplate)
+        </a>
+        <a className="list-group-item" href="https://firebase.google.com/" target="_blank">
+          Firebase
+        </a>
+      </div>
     </div>
   );
 };
--- src/modules/common/HomePage.js
+++ src/modules/common/HomePage.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import {Link, IndexLink} from 'react-router';
 
 import {APP_TITLE} from '../../constants/appConfig';
+import SideNav from '../layout/components/SideNav';
 
 
 const HomePage = () => {
   return (
     <div>
-      <h1>{APP_TITLE}</h1>
+      <h3>{APP_TITLE}</h3>
+      <hr/>
+
+      <div className="list-group">
+        <Link to="/game" className="list-group-item">Games</Link>
+        <Link to="/platform" className="list-group-item">Platforms</Link>
+        <Link to="/account" className="list-group-item">Account</Link>
+        <Link to="/about" className="list-group-item">About</Link>
+      </div>
+
     </div>
   );
 };
